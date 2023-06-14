0682c6925cfa0e88520517e7951057ea6fc43605
yunss-ML <younsselbrg@gmail.com>
2023-06-14 16:57:21 +0100

    the Class engine AutoGrad doen

--- core/engine.py
+++ core/engine.py
@@ -1,15 +1,18 @@
 import numpy as np 
 import math 
+import random
 
 
 class Value:
     """ stores a single scalar value and its gradient """
 
     def __init__(self, data, _children=(), _op=''):
+        if data is None:
+          data = random.uniform(-1,1)
         self.data = data
         self.grad = 0
         # internal variables used for autograd graph construction
-        self._backward = lambda: None
+        self._backward = lambda *a, **k: None
         self._prev = set(_children)
         self._op = _op # the op that produced this node, for graphviz / debugging / etc
 
@@ -17,7 +20,7 @@ class Value:
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
-        def _backward():
+        def _backward(keep_graph=False):
             self.grad += out.grad
             other.grad += out.grad
         out._backward = _backward
@@ -28,9 +31,13 @@ class Value:
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
-        def _backward():
-            self.grad += other.data * out.grad
-            other.grad += self.data * out.grad
+        def _backward(keep_graph=False):
+            if keep_graph:
+                self.grad += other * out.grad
+                other.grad += self * out.grad
+            else:
+                self.grad += other.data * out.grad
+                other.grad += self.data * out.grad
         out._backward = _backward
 
         return out
@@ -39,8 +46,11 @@ class Value:
         assert isinstance(other, (int, float)), "only supporting int/float powers for now"
         out = Value(self.data**other, (self,), f'**{other}')
 
-        def _backward():
-            self.grad += (other * self.data**(other-1)) * out.grad
+        def _backward(keep_graph=False):
+            if keep_graph:
+                self.grad += (other * self**(other-1)) * out.grad
+            else:
+                self.grad += (other * self.data**(other-1)) * out.grad
         out._backward = _backward
 
         return out
@@ -48,12 +58,12 @@ class Value:
     def relu(self):
         out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
 
-        def _backward():
+        def _backward(keep_graph=False):
             self.grad += (out.data > 0) * out.grad
         out._backward = _backward
 
         return out
-        
+    
     def softmax(self):
 
         out =  Value(np.exp(self.data) / np.sum(np.exp(self.data), axis=1)[:, None], (self,), 'softmax')
@@ -67,23 +77,26 @@ class Value:
 
         return out
 
-    def backward(self):
-
+    def backward(self, keep_graph=False):
         # topological order all of the children in the graph
         topo = []
         visited = set()
         def build_topo(v):
             if v not in visited:
                 visited.add(v)
+                # Set to grad to zero to prevent previous values effecting the
+                # result.
+                v.grad = 0
                 for child in v._prev:
                     build_topo(child)
                 topo.append(v)
         build_topo(self)
 
-        # go one variable at a time and apply the chain rule to get its gradient
-        self.grad = 1
+        # Making grad a `Value` type allows us to track backprop and do higher
+        # order gradients.
+        self.grad = Value(1) if keep_graph else 1
         for v in reversed(topo):
-            v._backward()
+            v._backward(keep_graph=keep_graph)
 
     def __neg__(self): # -self
         return self * -1
@@ -108,3 +121,8 @@ class Value:
 
     def __repr__(self):
         return f"Value(data={self.data}, grad={self.grad})"
+        
+    
+
+   
+   
