a3f9747273df6396ce905ac5c7976c33ed3daa10
yunss-ML <younsselbrg@gmail.com>
2023-06-23 21:59:56 +0100

    publish package

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Nano-AutoGrad",
+    name="ano-autograd",
     version="0.1.0",
     author="Youness EL BRAG",
     author_email="younsselbrag@gmail.com",
     description="A tinyTroch scalar-Engine Nano-autograd a Micro-Framework with a small PyTorch-like neural network library on top.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/deep-matter/Nano-AutoGrad",
+    url="https://github.com/deep-matter/Nano-AutoGrad/autograd",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
