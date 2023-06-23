434e0b61f9cda8ee038eaece3e9aa504abfcc16e
yunss-ML <younsselbrg@gmail.com>
2023-06-23 23:04:09 +0100

    publish package and created PIP v0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nano-autograd",
+    name="tinyTroch",
     version="0.1.0",
     author="Youness EL BRAG",
     author_email="younsselbrag@gmail.com",
